4f1afe1de09aedebceaa1dfcfe49b6566a8656e6
shirashiki <silvio.hirashiki@yahoo.ca>
2014-07-25 22:17:31 -0400

    test

--- Learning/app/src/main/java/com/example/shirashiki/learning/MyActivity.java
+++ Learning/app/src/main/java/com/example/shirashiki/learning/MyActivity.java
@@ -10,6 +10,7 @@ public class MyActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
     }
